d82b71e4d62038f386cc1a99a11d17453306a2f3
Kevin Dong <kevin@jewlr.com>
2019-09-05 16:56:18 -0400

    added sign in features

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+
+  before_save :create_remember_token
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
 end
