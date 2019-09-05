6961a916578570bc8f023bb4f04cada82d811986
Kevin Dong <kevin@jewlr.com>
2019-09-05 14:34:10 -0400

    added sign up features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'welcome/index'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :todo
+  resources :users
 
   root 'welcome#index'
 
