216f1bfda4131895933ed6683066ec3b611eff54
Kevin Dong <kevin@jewlr.com>
2019-09-05 17:31:22 -0400

    bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
 
-  before_save :create_remember_token
+  
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
