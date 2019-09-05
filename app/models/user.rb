25b257bb47527148346f0a8176e9e41bc1033db7
Kevin Dong <kevin@jewlr.com>
2019-09-05 14:44:50 -0400

    added instructions on the validation process

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   before_save { |user| user.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
+
+  #regular expression for an valid email address
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
