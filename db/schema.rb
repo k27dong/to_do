daccbdac0d235274b902f238c14f6ac8b22801aa
Kevin Dong <kevin@jewlr.com>
2019-09-05 09:43:17 -0400

    changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_04_195049) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "content"
