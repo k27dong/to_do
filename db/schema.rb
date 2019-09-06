7ecc5182477e9d93dd88058c6a02e34447ed984b
Kevin Dong <kevin@jewlr.com>
2019-09-06 09:40:15 -0400

    removed duplicate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_203619) do
+ActiveRecord::Schema.define(version: 2019_09_06_133914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "motto"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "ddl"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "motto"
+    t.string "password_digest"
   end
 
 end
